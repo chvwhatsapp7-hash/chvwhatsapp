df75af6bf1cf7f5a0e38286883777fe341f42b1a
JoshiSankar-4021 <joshisankar5051@gmail.com>
2026-01-30 17:49:06 +0530

    Login jsx and reset passwrod

--- src/pages/auth/login.jsx
+++ src/pages/auth/login.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import LoginForm from "../../components/Auth/LoginForm";
+import LoginForm from "../../components/Authentication/LoginForm";
 
 function LoginPage () {
     return (
